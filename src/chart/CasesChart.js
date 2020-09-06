50722308378fc9c6c5d4795d66f4204d3eb65d42
Michael Bowman <bowmanmc@gmail.com>
2020-09-06 10:42:17 -0400

    chart labels

--- src/chart/CasesChart.js
+++ src/chart/CasesChart.js
@@ -19,11 +19,17 @@ export default ({ data, fips }) => {
     const counts = data.counts[fips];
     const series = [];
     const start = moment(data.counts.start, 'YYYYMMDD');
+    let maxDate = '';
+    let maxFound = false;
     for (let i = 0; i < counts.cases.length; i++) {
         series.push({
             x: moment(start).add(i, 'days').toDate(),
             y: counts.cases[i],
         });
+        if (!maxFound && counts.cases[i] === data.counts[fips].maxCases) {
+            maxFound = true;
+            maxDate = moment(start).add(i, 'days').format('MM/DD/YYYY');
+        }
     }
 
     const ctx = {
@@ -33,10 +39,26 @@ export default ({ data, fips }) => {
     };
 
     return (
-        <svg className="CasesChart" viewBox={`0 0 ${WIDTH} ${HEIGHT}`}>
-            <MonthSeparators series={series} context={ctx} />
-            <Bars series={series} context={ctx} />
-            {/* <Events series={series} context={ctx} /> */}
-        </svg>
+        <div className="CasesChart">
+            <svg className="CasesChart" viewBox={`0 0 ${WIDTH} ${HEIGHT}`}>
+                <MonthSeparators series={series} context={ctx} />
+                <Bars series={series} context={ctx} />
+                {/* <Events series={series} context={ctx} /> */}
+            </svg>
+            <div className="CasesChart__legend">
+                <div className="CasesChart__chartlabel">
+                    Count of Daily New Cases
+                </div>
+                <div className="CasesChart__maxlabel">
+                    <div className="CasesChart__maxcolor"></div>
+                    <div className="CasesChart__maxtext">
+                        Highest
+                        ({ctx.max.toLocaleString()})
+                        on { maxDate }
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 };
